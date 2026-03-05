bfddfad7304ccd8cf9c643879f7495f7a417ab25
Sean Jhanz <seanjhanz111@gmail.com>
2026-03-05 08:49:53 +0800

    reset commit

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+require_once "controllers/template.controller.php"; //will enter the controllers folder and get the file controller.php
+$template = new ControllerTemplate(); // class to be instiatied in index here / nameObj = $template
+$template -> ctrTemplate();
+?>
